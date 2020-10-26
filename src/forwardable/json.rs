588ac1bdaaf4be2681c559bb9151814d0e3c4f84
hyeok.jeon <coworker.ai@gmail.com>
2020-10-26 17:39:26 +0900

    aa

--- src/forwardable/json.rs
+++ src/forwardable/json.rs
@@ -31,22 +31,22 @@ use time;
 
 impl<'a, A: ToSocketAddrs> JsonForwardable for Fluent<'a, A> {
     /// Post record into Fluentd. Without time version.
-    fn post<T>(self, record: T, max_retry: u64, multiplier: f64) -> Result<(), FluentError>
+    fn post<T>(self, record: T) -> Result<(), FluentError>
     where
         T: Serialize + Debug + Clone,
     {
         let time = time::now();
-        self.post_with_time(record, time, max_retry, multiplier)
+        self.post_with_time(record, time)
     }
 
     /// Post record into Fluentd. With time version.
-    fn post_with_time<T>(self, record: T, time: time::Tm, max_retry: u64, multiplier: f64) -> Result<(), FluentError>
+    fn post_with_time<T>(self, record: T, time: time::Tm) -> Result<(), FluentError>
     where
         T: Serialize + Debug + Clone,
     {
         let record = Record::new(self.get_tag().into_owned(), time, record);
         let addr = self.get_addr();
-        //let (max, multiplier) = self.get_conf().into_owned().build();
+        let (max_retry, multiplier) = self.get_conf().into_owned().build();
         match retry_exponentially(
             max_retry,
             multiplier,
